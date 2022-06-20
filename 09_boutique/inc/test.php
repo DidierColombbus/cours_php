1788450b7b94c89faa0d442386d72f938333e78c
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-20 16:00:21 +0200

    Create test.php

---
+++ 09_boutique/inc/test.php
@@ -0,0 +1,16 @@
+<?php
+
+    $host = 'localhost';
+    $database = 'site';
+    $user = 'root';
+    $psw = '';
+
+    $pdoSITE = new PDO('mysql:host=' .$host. 'dbname=' .$database, $user, $psw);
+    $pdoSITE->exec('SET NAMES utf8');
+    // echo 'coucou';
+    $requete = $pdoSITE->query("SELECT * FROM membre");
+    $ligne = $requete->fetch(PDO::FETCH_ASSOC);
+
+    require_once('functions.php')
+
+?>
