3d26574cffb6941737d8dd735fe1c1373073250a
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-20 16:30:09 +0200

    Update test.php

--- 09_boutique/inc/test.php
+++ 09_boutique/inc/test.php
@@ -5,12 +5,23 @@
     $user = 'root';
     $psw = '';
 
-    $pdoSITE = new PDO('mysql:host=' .$host. 'dbname=' .$database, $user, $psw);
+    $pdoSITE = new PDO('mysql:host=' .$host. ';dbname=' .$database, $user, $psw);
     $pdoSITE->exec('SET NAMES utf8');
     // echo 'coucou';
     $requete = $pdoSITE->query("SELECT * FROM membre");
     $ligne = $requete->fetch(PDO::FETCH_ASSOC);
 
-    require_once('functions.php')
+    require_once('functions.php');
+
+    echo "<p>Les infos de notre premier memebre:</p>";
+    echo "<ul>";
+    echo "<li>Prénom : " .$ligne['prenom'] . "</li><li>Nom : ".$ligne['nom']."</li><li>Pseudo : " .$ligne['pseudo']."</li><li>Adresse : ".$ligne['adresse']. "</li><li>Code postal : ".$ligne['code_postal']."</li><li>Sexe : ";
+    
+    if ($ligne['civilite'] == 'f'){
+        echo "Féminin";
+    }else {
+        echo "Masculin";
+    }
+    echo "</li></ul>";
 
 ?>
