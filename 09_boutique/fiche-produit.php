cf838dfc04d9f021df41ed35f8db020282d12f83
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-23 12:11:15 +0200

    Update fiche-produit.php

--- 09_boutique/fiche-produit.php
+++ 09_boutique/fiche-produit.php
@@ -3,7 +3,7 @@
     
     
     if(isset($_GET['id_produit'])){
-        $resultat = $pdoSITE->prepare("SELECT * FROM produit WHERE id_produit = :id_produit");
+        $resultat = $pdoSITE->prepare("SELECT * FROM site.produit WHERE id_produit = :id_produit");
         $resultat->execute(array(
             ':id_produit'=>$_GET['id_produit']
         ));
@@ -34,8 +34,38 @@
   <body>
       <main class="container bg-white m-4 mx-auto p-4">
         <div class="row">
-            <div class="col-sm-12 col-md-4">
-                <h1>La boutique</h1>
+                <div class="col-sm-12">
+                <h1 class="text-center">La boutique - Découvrez notre <?php echo $fiche['titre'] ?>!</h1>
+
+                <div class="card mx-auto" style="width: 18rem;">
+                    <img src="<?php echo $fiche['photo'];?>" class="card-img-top">
+            
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><?php echo $fiche['categorie']. " - " .$fiche['couleur']; ?></h5>
+
+                        <p class="card-text"><?php echo "Description : " .ucfirst($fiche['description']). "<br> Taille : " .ucfirst($fiche['taille']). "<br> Public : ";
+                        if($fiche['public'] == 'f') {
+                            echo "Féminin ";
+                        }else if ($fiche['public'] == 'm'){
+                            echo "Masculin";
+                        }else {
+                            echo "Mixte";
+                        }
+                        $fmt = new NumberFormatter( 'ru_RU', NumberFormatter::CURRENCY );
+                        echo "<br>Prix : " .$fmt->formatCurrency($fiche['prix'], "EUR"). "</p>";
+                        if ($fiche['stock'] < 1) {
+                            echo "<p class=\"alert alert-danger\">Ce produit n'est plus en stock !</p>";
+                        }elseif ($fiche['stock'] < 4) {
+                            echo "<p class=\"alert alert-warning\">Attention, il ne reste presque plus de " . strtolower($fiche['titre']) . " ! Plus que " .$fiche['stock']. " " .strtolower($fiche['categorie']) . "s en stock.</p>";
+                        }elseif ($fiche['stock'] == 1) {
+                            echo "<p class=\"alert alert-warning\">Attention, il ne reste qu'un" .strtolower($fiche['categorie']) . " en stock.</p>";
+                        }else {
+                        echo "<p class=\"alert alert-success\">Encore en stock : ". $fiche['stock']. "</p>";
+                        } ?>  
+                    </div> <!-- fin card body -->
+                </div><!-- fin card -->
+                <p>Retourner à <a href="03-produits.php">tous nos produits</a></p>
+                
             </div>
         </div>
 
