355b6665f743b05062b7cf99c5bbc95cbf8572cd
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-23 12:03:00 +0200

    update

--- 09_boutique/03-produits.php
+++ 09_boutique/03-produits.php
@@ -1,12 +1,12 @@
 <?php
-    require_once('inc/init.php');
+    require_once('./inc/init.php');
     // jeprint_r($_SESSION);
 ?>
 
 <!doctype html>
 <html lang="fr">
   <head>
-    <title>La boutique - nos produits</title>
+    <title>La boutique - Nos produits</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,40 +16,39 @@
 
   </head>
   <body>
-      <main class="container bg-white m-4 mx-auto p-4">
+    <main class="container bg-white m-5 mx-auto p-4">
         <div class="row">
             <div class="col-sm-12">
-                <h1>La boutique - Décrouvrez nos produits!</h1>
+                <h1>La boutique - Découvrez nos produits!</h1>
                 <?php
-                    $requete = $pdoSITE->query("SELECT * FROM produit ORDER BY id_produit");
-                    echo "<table class='table table-striped'>";
-                    echo "<thead><tr><th scope='col'>ID_produit</th><th scope='col'>Visuel</th><th scope='col'>Référence</th><th scope='col'>Titre</th><th scope='col'>Public</th><th scope='col'>Prix</th><th scope='col'>Fiche</th></tr></thead>";
-
+                    $requete = $pdoSITE->query("SELECT * FROM site.produit ORDER BY id_produit");
+                    echo "<table class=\"table table-striped\">";
+                    echo "<thead><tr><th scope=\"col\">ID_produit</th><th scope=\"col\">Visuel</th><th scope=\"col\">Référence</th><th scope=\"col\">Titre</th><th scope=\"col\">Public</th><th scope=\"col\">Prix</th><th scope=\"col\">Fiche</th></tr></thead>";
                     while($ligne = $requete->fetch(PDO::FETCH_ASSOC)){
                         echo "<tr>";
-                        echo "<td>#" . $ligne['id_produit'] . "</td>";
-                        echo "<td><img src='" . $ligne['photo'] . "' width='200'></td>";
-                        echo "<td>" . $ligne['reference'] . "</td>";
-                        echo "<td>" . $ligne['titre'] . "</td>";
+                        echo "<td>#". $ligne['id_produit'] . "</td>";
+                        echo "<td><img src=\"". $ligne['photo'] ."\" width=\"200\"></td>";
+                        echo "<td>". $ligne['reference'] . "</td>";
+                        echo "<td>". $ligne['titre'] . "</td>";
                         echo "<td>";
                         if($ligne['public'] == 'f'){
-                            echo "Féminin";
+                            echo "Feminin";    
                         }else if($ligne['public'] == 'm'){
-                            echo "Masculin";
-                        }else{
-                            echo "Mixte";
-                        }
-                        $fmt = new NumberFormatter( 'ru_RU', NumberFormatter::CURRENCY );
-                        echo "<td>" . $fmt->formatCurrency($ligne['prix'], "EUR") . "</td>";
-                        echo "<td><a href='fiche-produit.php?id_produit=" . $ligne['id_produit'] . "' class='text-dark'>Voir sa fiche</a></td>";
-                        echo "</tr>";
+                            echo "Masculin"; 
+                            }else{
+                                echo "Mixte";
+                            }
+                            $fmt = new NumberFormatter('ru_RU', NumberFormatter:: CURRENCY);
+                            echo "<td>".$fmt->formatCurrency($ligne['prix'], "EUR"). "</td>";
+                            echo "<td><a href=\"fiche-produit.php?id_produit=".$ligne['id_produit']."\" class=\"text-dark\">Voir sa fiche</a></td>";
+                            echo "</tr>";
                     }
                     echo "</table>";
                 ?>
             </div>
-        </div>
-
-      </main>
+        </div><!--fin de la row(rangée) -->
+    </main>
+      
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 
