b412b04af0fa459cc4dc6a8188887e0ba634a72a
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-23 09:32:08 +0200

    update

--- 09_boutique/01-inscription.php
+++ 09_boutique/01-inscription.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('./init.php');
+    require_once('./inc/init.php');
     jeprint_r($_SESSION);
 
     if(!empty($_POST)){
@@ -51,23 +51,23 @@
                 <form action="" method="POST" class="w-75 mx-auto">
                   <div class="form-group">
                     <label for="pseudo">Pseudo</label>
-                    <input type="text" class="form-control" id="pseudo" name="pseudo" value="<?php echo $_POST['pseudo'] ??'';?>" required>
+                    <input type="text" class="form-control" id="pseudo" name="pseudo" required>
                   </div>
                   <div class="form-group">
                     <label for="mdp">Mot de passe</label>
-                    <input placeholder="Votre mot de passe doit contenir entre 8 et 20 caractères." type="password" class="form-control" id="mdp" name="mdp" value="<?php echo $_POST['mdp'] ??'';?>" required>
+                    <input placeholder="Votre mot de passe doit contenir entre 8 et 20 caractères." type="password" class="form-control" id="mdp" name="mdp" required>
                   </div>
                   <div class="form-group">
                     <label for="nom">Nom</label>
-                    <input type="text" class="form-control" id="nom" name="nom" value="<?php echo $_POST['nom'] ??'';?>" required>
+                    <input type="text" class="form-control" id="nom" name="nom" required>
                   </div>
                   <div class="form-group">
                     <label for="prenom">Prénom</label>
-                    <input type="text" class="form-control" id="prenom" name="prenom" value="<?php echo $_POST['prenom'] ??'';?>" required>
+                    <input type="text" class="form-control" id="prenom" name="prenom" required>
                   </div>
                   <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control" id="mail" name="mail" value="<?php echo $_POST['mail'] ??'';?>" required>
+                    <input type="email" class="form-control" id="mail" name="mail" required>
                   </div>
                   <div class="form-group">
                     <label for="civilite">Genre</label>
@@ -79,15 +79,15 @@
                   </div>
                   <div class="form-group">
                     <label for="adresse">Adresse</label>
-                    <input type="text" class="form-control" id="adresse" name="adresse" value="<?php echo $_POST['adresse'] ??'';?>" required>
+                    <input type="text" class="form-control" id="adresse" name="adresse" required>
                   </div>
                   <div class="form-group">
                     <label for="code_postal">Code postal</label>
-                    <input type="text" class="form-control" id="code_postal" name="code_postal" value="<?php echo $_POST['code_postal'] ??'';?>" required>
+                    <input type="text" class="form-control" id="code_postal" name="code_postal" required>
                   </div>
                   <div class="form-group">
                     <label for="ville">Ville</label>
-                    <input type="text" class="form-control" id="ville" name="ville" value="<?php echo $_POST['ville'] ??'';?>" required>
+                    <input type="text" class="form-control" id="ville" name="ville" required>
                   </div>
                   <input type="submit" class="btn btn-secondary my-2">
                   <input type="reset" class="btn btn-secondary my-2">
