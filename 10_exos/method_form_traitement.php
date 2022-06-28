fad1901689d2ae29cd3cc269ca44b8b732c19281
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-28 11:41:36 +0200

    Update method_form_traitement.php

--- 10_exos/method_form_traitement.php
+++ 10_exos/method_form_traitement.php
@@ -6,11 +6,11 @@
         echo"<p>Nom : " . $_POST['nom'] . "</p>";
         echo"<p>Commentaire : " . $_POST['commentaire'] . "</p>";
 
-        // on va écrire le contenu de la superglobale dans un fichier texteen l'absence de BDD
+        // on va écrire le contenu de la superglobale dans un fichier texte en l'absence de BDD
 
         $fichier = fopen('formulaire.txt', 'a'); // fopenn() en mode 'a' permet de créer un fichier s'il n'existe pas encore, sinon cela permet de l'ouvrir
 
-        $donneeformulaire = $_POST['prenom']. " " .$_POST['nom']. " " .$_POST['commentaire'] . "\n"; // \n pour faier des retours à la ligne dans le .txt
+        $donneeformulaire = $_POST['prenom']. " " .$_POST['nom']. " " .$_POST['commentaire'] . "\n"; // \n pour faire des retours à la ligne dans le .txt
 
         fwrite($fichier, $donneeformulaire);
     }
